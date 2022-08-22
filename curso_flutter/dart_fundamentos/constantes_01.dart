0e972b4840089790d3b9507a4de3634e5c1144ba
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-22 14:23:34 -0300

    Tipos Básicos 01

--- curso_flutter/dart_fundamentos/constantes_01.dart
+++ curso_flutter/dart_fundamentos/constantes_01.dart
@@ -3,15 +3,15 @@ import 'dart:io';
 main() {
   // Área da circunferência = PI * raio * raio
 
-  const double pi = 3.1415;
+  const double PI = 3.1415;
   const x = 3;
-  const y = x * pi;
+  const y = x * PI;
 
   stdout.write("Informe o raio: ");
   String entradaDoUsuario = stdin.readLineSync()!;
   final double raio = double.parse(entradaDoUsuario);
 
-  final double area = pi * (raio * raio);
+  final double area = PI * (raio * raio);
 
   print("O valor da área é: " + area.toString());
 }
