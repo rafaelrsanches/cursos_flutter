382da806ef3266a700adfd5470e9e833495e13d6
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-23 10:47:24 -0300

    Usando Generics

---
+++ curso_flutter/dart_fundamentos/generics.dart
@@ -0,0 +1,15 @@
+main() {
+  List<String> frutas = ['Banana', 'Maçâ', 'Laranja'];
+  frutas.add('Pêra');
+
+  print(frutas);
+
+  Map<String, double> salarios = {
+    "Gerente": 19800,
+    "Vendedor": 15000,
+    "Estagiário": 1800,
+    "Dev": 30000,
+  };
+
+  print(salarios);
+}
