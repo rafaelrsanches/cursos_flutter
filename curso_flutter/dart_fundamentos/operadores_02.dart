d6d1d6bbf506d18eaf0d7aed94cf2ef73be50df6
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-23 08:47:55 -0300

    Operadores 02

---
+++ curso_flutter/dart_fundamentos/operadores_02.dart
@@ -0,0 +1,23 @@
+main() {
+  // Operadores de Atribuição
+  double a = 2;
+  a = a + 3;
+  a += 3;
+  a -= 3;
+  a *= 3;
+  a /= 3;
+  a %= 2;
+
+  print(a);
+
+  // Operadores relacionais
+  print(3 > 2);
+  print(3 >= 3);
+  print(3 < 4);
+  print(3 <= 3);
+  print(3 != 3);
+  print(3 == 3);
+  print(3 == '3');
+
+  print(2 + 5 > 3 - 1 && 4 + 7 != 7 - 4);
+}
