08d537452b6253242672be439873fb758abd1f26
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-26 13:35:16 -0300

    Retornar Função

---
+++ curso_flutter/dart_funcoes/retornar_funcao.dart
@@ -0,0 +1,14 @@
+main() {
+  print(somaParcial(2)(10));
+
+  var somaCom10 = somaParcial(10);
+
+  print(somaCom10(5));
+}
+
+int Function(int) somaParcial(int a) {
+  int c = 0;
+  return (int b) {
+    return (a + b);
+  };
+}
