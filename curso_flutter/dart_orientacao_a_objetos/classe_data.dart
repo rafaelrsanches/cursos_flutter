258a860e04dd934d00da50ce7bec7c3379474da9
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-29 14:25:23 -0300

    O Básico sobre Classe

---
+++ curso_flutter/dart_orientacao_a_objetos/classe_data.dart
@@ -0,0 +1,20 @@
+class Data {
+  int? dia;
+  int? mes;
+  int? ano;
+}
+
+main() {
+  var dataAniversario = new Data();
+  dataAniversario.dia = 18;
+  dataAniversario.mes = 04;
+  dataAniversario.ano = 2014;
+
+  Data dataCompra = new Data();
+  dataCompra.dia = 1;
+  dataCompra.mes = 1;
+  dataCompra.ano = 1970;
+
+  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+}
