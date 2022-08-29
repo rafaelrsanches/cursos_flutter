4ebf081b4b30588d570f75b5e4ad913c14bd83ce
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-29 15:22:34 -0300

    Construtores

--- curso_flutter/dart_orientacao_a_objetos/classe_data.dart
+++ curso_flutter/dart_orientacao_a_objetos/classe_data.dart
@@ -3,6 +3,15 @@ class Data {
   int? mes;
   int? ano;
 
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+  Data.com({this.dia = 1, this.mes = 1, this.ano = 1970});
+
   String obterDataFormatada() {
     return "$dia/$mes/$ano";
   }
@@ -13,12 +22,12 @@ class Data {
 }
 
 main() {
-  var dataAniversario = new Data();
-  dataAniversario.dia = 18;
-  dataAniversario.mes = 04;
-  dataAniversario.ano = 2014;
+  var dataAniversario = new Data(18, 04, 2014);
+  //dataAniversario.dia = 18;
+  //dataAniversario.mes = 04;
+  //dataAniversario.ano = 2014;
 
-  Data dataCompra = new Data();
+  Data dataCompra = new Data(01, 01, 1969);
   dataCompra.dia = 1;
   dataCompra.mes = 1;
   dataCompra.ano = 1970;
