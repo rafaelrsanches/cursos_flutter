44e2ee026ed1d9befa6e63260baed3de5e6055fb
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-02 16:02:03 -0300

    Variáveis em StatelessWidget

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  int perguntaSelecionada = 0;
+
   void responder() {
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
     print("Pergunta respondida");
   }
 
@@ -21,20 +25,18 @@ class PerguntaApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text(perguntas[0]),
+              Text(perguntas[perguntaSelecionada]),
               ElevatedButton(
                 child: Text("Resposta 1"),
                 onPressed: responder,
               ),
               ElevatedButton(
                 child: Text("Resposta 2"),
-                onPressed: () {
-                  print("Resposta 2 selecionada!");
-                },
+                onPressed: responder,
               ),
               ElevatedButton(
                 child: Text("Resposta 3"),
-                onPressed: () => print("Resposta 3 selecionada!"),
+                onPressed: responder,
               )
             ],
           ),
