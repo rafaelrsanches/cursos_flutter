24637e788de7a41652332d670b71821abd782c1d
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-05 15:25:50 -0300

    Passando Função Callback

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -12,7 +12,6 @@ class _perguntaAppState extends State<PerguntaApp> {
     setState(() {
       _perguntaSelecionada++;
     });
-    print(_perguntaSelecionada);
   }
 
   final List<String> perguntas = [
@@ -30,9 +29,9 @@ class _perguntaAppState extends State<PerguntaApp> {
           body: Column(
             children: [
               Questao(perguntas[_perguntaSelecionada]),
-              Resposta("Resposta 1"),
-              Resposta("Resposta 2"),
-              Resposta("Resposta 3"),
+              Resposta("Resposta 1", _responder),
+              Resposta("Resposta 2", _responder),
+              Resposta("Resposta 3", _responder),
             ],
           ),
         ),
