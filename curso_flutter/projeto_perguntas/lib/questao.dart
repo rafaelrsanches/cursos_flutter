77ae615a4f3f69cf3927bda4a4632c5ef34561e6
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-05 14:25:34 -0300

    Criando Widget Questão

---
+++ curso_flutter/projeto_perguntas/lib/questao.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class Questao extends StatelessWidget {
+  final String texto;
+
+  Questao(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(texto);
+  }
+}
