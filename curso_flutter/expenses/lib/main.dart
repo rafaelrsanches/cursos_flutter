c1d8c0d9635b81be4ecac48872b724585502d3a7
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-12 16:07:36 -0300

    Criando Classe de Modelo (Transaction)

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -10,6 +11,20 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: "t1",
+        title: "Novo tênis de corrida",
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+      id: "t2",
+      title: "Conta de Luz",
+      value: 295.30,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
