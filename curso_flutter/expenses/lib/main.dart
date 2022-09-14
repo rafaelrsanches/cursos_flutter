52debff2c4ec918c1d5f2c98ba49af49a3688f4f
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-14 14:55:38 -0300

    Instalando Pacote Externo Intl

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -77,7 +78,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat("d MMM y").format(tr.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
