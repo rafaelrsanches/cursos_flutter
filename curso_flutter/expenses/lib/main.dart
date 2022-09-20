d4a2dd3bf904f332c4709e930abe2c9ad4fb8811
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-20 09:22:14 -0300

    Adicionar Imagem

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -42,18 +42,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
-    Transaction(
-        id: "t1",
-        title: "Novo tênis de corrida",
-        value: 310.76,
-        date: DateTime.now()),
-    Transaction(
-      id: "t2",
-      title: "Conta de Luz",
-      value: 295.30,
-      date: DateTime.now(),
-    ),
+  final List<Transaction> _transactions = [
+    // Transaction(
+    //     id: "t1",
+    //     title: "Novo tênis de corrida",
+    //     value: 310.76,
+    //     date: DateTime.now()),
+    // Transaction(
+    //   id: "t2",
+    //   title: "Conta de Luz",
+    //   value: 295.30,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   _addTransaction(String title, double value) {
