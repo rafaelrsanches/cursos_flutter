30e5ba3be3caff7868b2bf7f09aa38c84d3037fe
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-10-25 08:41:58 -0300

    Transação com Data

--- curso_flutter/expenses/lib/components/transaction_form.dart
+++ curso_flutter/expenses/lib/components/transaction_form.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
-  final void Function(String, double) onSubmit;
+  final void Function(String, double, DateTime) onSubmit;
 
   TransactionForm(this.onSubmit);
 
@@ -15,17 +15,17 @@ class TransactionForm extends StatefulWidget {
 class _TransactionFormState extends State<TransactionForm> {
   final _titleController = TextEditingController();
   final _valueController = TextEditingController();
-  var _selectedDate;
+  var _selectedDate = DateTime.now();
 
   _submitForm() {
     final String title = _titleController.text;
     final double value = double.tryParse(_valueController.text) ?? 0.0;
 
-    if (title.isEmpty || value <= 0) {
+    if (title.isEmpty || value <= 0 || _selectedDate == null) {
       return;
     }
 
-    widget.onSubmit(title, value);
+    widget.onSubmit(title, value, _selectedDate);
   }
 
   _showDatePicker() {
